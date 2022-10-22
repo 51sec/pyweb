5e440e0eb95d88b33dd78cba814b0188d88ab9b6
51sec <johnyan.ca@gmail.com>
2022-10-22 18:26:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ def home_view():
         ip_addr1 = request.remote_addr
         ip_addr2 = request.environ['REMOTE_ADDR']
         ip_addr3 = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-        return "<h1>Welcome to My PY website!</h1>"+"</BR>"+"<h1> Your IP address is:" + ip_addr1 +"</BR>" + "<h1> Your IP address is:" + ip_addr2 + "</BR>" + "<h1> Your IP address is:" + ip_addr3
+        return "</BR>" +"</BR>" +"</BR>" +"<h1>Welcome to My PY website!</h1>"+"</BR>" +
+            "<h1> Your IP address is:" + ip_addr1 +"</BR>" + 
+            "<h1> Your IP address is:" + ip_addr2 + "</BR>" + 
+            "<h1> Your IP address is:" + ip_addr3
