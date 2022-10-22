ede9382b701fcb31ca5dffeb539c54a9ff812a45
51sec <johnyan.ca@gmail.com>
2022-10-22 17:55:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,20 @@ app = Flask(__name__)
   
 @app.route("/") 
 def home_view():
+        return "<h1>Welcome to My PY website!</h1>"
+    
+def home_view():
         ip_addr = request.remote_addr
         return '<h1> Your IP address is:' + ip_addr +"</BR>"
-        
-        return "<h1>Welcome to My PY website!</h1>"
-  
+
+@app.route('/client')
+def client():
+    ip_addr = request.environ['REMOTE_ADDR']
+    return '<h1> Your IP address is:' + ip_addr
+
+
+@app.route('/client')
+def proxy_client():
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    return '<h1> Your IP address is:' + ip_addr
+
