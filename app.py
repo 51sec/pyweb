9a74b4a21a547d28e049de60e5c7011c5243c783
51sec <johnyan.ca@gmail.com>
2022-10-22 17:42:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # sample-web-app/app.py
 from flask import Flask 
-  
+from http.server import BaseHTTPRequestHandler
+
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view(): 
         return "<h1>Welcome to My website!</h1>"
+  
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(self.headers.get('x-forwarded-for').encode())
+    return
