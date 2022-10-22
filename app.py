90af676f7fcd28c8b12c3c37b37b0b3a496cfeb5
51sec <johnyan.ca@gmail.com>
2022-10-22 17:49:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 # sample-web-app/app.py
-from flask import Flask 
-from http.server import BaseHTTPRequestHandler
+from flask import Flask
 
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view(): 
+        return '<h1> Your IP address is:' + ip_addr +"</BR>"
         return "<h1>Welcome to My PY website!</h1>"
   
-class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.end_headers()
-    self.wfile.write(self.headers.get('x-forwarded-for').encode())
-    return self.headers.get('x-forwarded-for').encode()
